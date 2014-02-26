48b8452e55bc34503756279f17067f840ab23dc1
raphaelpetrini <raphael@factoryx.com.au>
2014-02-26 21:53:43 +1100

    bug fixes

--- app/code/local/FactoryX/Contests/sql/contests_setup/upgrade-0.7.1-0.7.2.php
+++ app/code/local/FactoryX/Contests/sql/contests_setup/upgrade-0.7.1-0.7.2.php
@@ -5,12 +5,16 @@ $installer = $this;
 $installer->startSetup();
 
 // Modify the minimum word count column into a maximum word count
-$installer
-	->getConnection()
-	->changeColumn (
-		$this->getTable('contests/contest'), 'minimum_word_count', 'maximum_word_count', 'smallint(2) default NULL'
-	);
+$conn = $installer->getConnection();
+$contestTable = $this->getTable('contests/contest');
 
+// Add first name
+if ($conn->tableColumnExists($contestTable, 'minimum_word_count')) 
+{
+	$conn->changeColumn (
+			$contestTable, 'minimum_word_count', 'maximum_word_count', 'smallint(2) default NULL'
+		);
+}
 // Remove old reference to competitions module
 $installer->run("DELETE FROM core_resource where code = 'competitions_setup';");
 	
