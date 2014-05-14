0e9f9de5f876d104b8e972bd6020db6e7864222b
Alvin Nguyen <alvin@alvinnguyen.net>
2014-05-14 11:14:35 +1000

    add cms block for email

--- scripts/cms_block_email.sql
+++ scripts/cms_block_email.sql
@@ -14,24 +14,6 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `cms_block`
---
-
-DROP TABLE IF EXISTS `cms_block`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `cms_block` (
-  `block_id` smallint(6) NOT NULL AUTO_INCREMENT COMMENT 'Block ID',
-  `title` varchar(255) NOT NULL COMMENT 'Block Title',
-  `identifier` varchar(255) NOT NULL COMMENT 'Block String Identifier',
-  `content` mediumtext COMMENT 'Block Content',
-  `creation_time` timestamp NULL DEFAULT NULL COMMENT 'Block Creation Time',
-  `update_time` timestamp NULL DEFAULT NULL COMMENT 'Block Modification Time',
-  `is_active` smallint(6) NOT NULL DEFAULT '1' COMMENT 'Is Block Active',
-  PRIMARY KEY (`block_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=113 DEFAULT CHARSET=utf8 COMMENT='CMS Block Table';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
