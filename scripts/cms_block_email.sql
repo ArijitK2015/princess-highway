6e7cb9a661050424b94b16e480052332ac7435c3
Alvin Nguyen <alvin@alvinnguyen.net>
2014-05-14 11:15:57 +1000

    add cms block for email

--- scripts/cms_block_email.sql
+++ scripts/cms_block_email.sql
@@ -14,7 +14,6 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `cms_block`
