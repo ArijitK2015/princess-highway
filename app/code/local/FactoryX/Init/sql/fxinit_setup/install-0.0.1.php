ac2a79af99310cc0bdeb9f11aa89e2b9dfb945bf
Alvin Nguyen <alvin@alvinnguyen.net>
2014-05-16 13:14:46 +1000

    update

--- app/code/local/FactoryX/Init/sql/fxinit_setup/install-0.0.1.php
+++ app/code/local/FactoryX/Init/sql/fxinit_setup/install-0.0.1.php
@@ -1,24 +1,36 @@
 <?php
-mail('alvin@factoryx.com.au','fx install','message');
 
+$email_content = "":
 
 $installer = $this;
 $installer->startSetup();
 
 //Import CMS Block
 $path = Mage::getBaseDir().'/app/code/local/FactoryX/Init/sql/mysqldump/'.'cms_block.sql';
-mail('alvin@factoryx.com.au','fx install',$path);
 if (file_exists($path)) {
 	$sql = file_get_contents($path);
 	mail('alvin@factoryx.com.au','fx install',$sql);
 	$installer->run($sql);
+	$email_content .= "Script ran for CMS block import<br/>";
+}else{
+	$email_content .= "Cannot find CMS block dump<br/>";
 }
 
 // Import Email Template
-
+$path = Mage::getBaseDir().'/app/code/local/FactoryX/Init/sql/mysqldump/'.'core_email_template.sql';
+if (file_exists($path)) {
+	$sql = file_get_contents($path);
+	mail('alvin@factoryx.com.au','fx install',$sql);
+	$installer->run($sql);
+	$email_content .= "Script ran for email template import<br/>";
+}else{
+	$email_content .= "Cannot find email template dump<br/>";
+}
 
 // Import CMS Page
 
+
+mail('alvin@factoryx.com.au','fx install','message');
 
 $installer->endSetup();
 
