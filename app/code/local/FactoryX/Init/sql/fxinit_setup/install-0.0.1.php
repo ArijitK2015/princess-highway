cf7c11c5d966ee3853fec14f2ae8867d1c72be53
Alvin Nguyen <alvin@alvinnguyen.net>
2014-05-16 12:01:45 +1000

    init

---
+++ app/code/local/FactoryX/Init/sql/fxinit_setup/install-0.0.1.php
@@ -0,0 +1,20 @@
+<?php
+$installer = $this;
+$installer->startSetup();
+
+// Import CMS Block
+// $path = Mage::getBaseDir().'app/code/local/FactoryX/Init/sql/mysqldump/'.'cms_block_email.sql';
+// if (file_exists($path)) {
+// 	$sql = file_get_contents($path);
+// 	//Mage::log($sql);
+// 	Mage::log('1');
+// }
+
+// Import Email Template
+
+
+// Import CMS Page
+
+asdasdasdsd
+
+$installer->endSetup();
