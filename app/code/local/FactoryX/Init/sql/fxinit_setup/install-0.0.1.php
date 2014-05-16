13f6c3070b732f6bfa4abbc15f0a17724f6035b0
Alvin Nguyen <alvin@alvinnguyen.net>
2014-05-16 13:09:40 +1000

    update

--- app/code/local/FactoryX/Init/sql/fxinit_setup/install-0.0.1.php
+++ app/code/local/FactoryX/Init/sql/fxinit_setup/install-0.0.1.php
@@ -1,11 +1,16 @@
 <?php
+mail('alvin@factoryx.com.au','fx install','message');
+
+
 $installer = $this;
 $installer->startSetup();
 
-..Import CMS Block
-$path = Mage::getBaseDir().'app/code/local/FactoryX/Init/sql/mysqldump/'.'cms_block_email.sql';
+//Import CMS Block
+$path = Mage::getBaseDir().'/app/code/local/FactoryX/Init/sql/mysqldump/'.'cms_block.sql';
+mail('alvin@factoryx.com.au','fx install',$path);
 if (file_exists($path)) {
 	$sql = file_get_contents($path);
+	mail('alvin@factoryx.com.au','fx install',$sql);
 	$installer->run($sql);
 }
 
@@ -16,3 +21,5 @@ if (file_exists($path)) {
 
 
 $installer->endSetup();
+
+?>
