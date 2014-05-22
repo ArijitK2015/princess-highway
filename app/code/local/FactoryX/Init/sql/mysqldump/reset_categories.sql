c6754b3caadfd86db07490dd64f487260fc529e3
Ben Incani <ben.incani@factoryx.com.au>
2014-05-22 16:44:58 +1000

    update init

---
+++ app/code/local/FactoryX/Init/sql/mysqldump/reset_categories.sql
@@ -0,0 +1,32 @@
+TRUNCATE TABLE `catalog_category_entity`;
+TRUNCATE TABLE `catalog_category_entity_datetime`;
+TRUNCATE TABLE `catalog_category_entity_decimal`;
+TRUNCATE TABLE `catalog_category_entity_int`; 
+TRUNCATE TABLE `catalog_category_entity_text`;
+TRUNCATE TABLE `catalog_category_entity_varchar`; 
+TRUNCATE TABLE `catalog_category_product`;
+TRUNCATE TABLE `catalog_category_product_index`;
+
+insert into `catalog_category_entity`(`entity_id`,`entity_type_id`,`attribute_set_id`,`parent_id`,`created_at`,`updated_at`,`path`,`position`,`level`,`children_count`)
+values
+(1, 3, 0, 0, NOW(),'0000-00-00 00:00:00', '1',   0, 0, 1),
+(2, 3, 3, 1, NOW(),'0000-00-00 00:00:00', '1/2', 1, 1, 0);
+
+insert into `catalog_category_entity_int`(`value_id`,`entity_type_id`,`attribute_id`,`store_id`,`entity_id`,`value`)
+values
+(1, 3, 67, 0, 1, 1),
+(2, 3, 67, 1, 1, 1),
+(3, 3, 42, 0, 2, 1),
+(4, 3, 67, 0, 2, 1),
+(5, 3, 42, 1, 2, 1),
+(6, 3, 67, 1, 2, 1);
+
+insert into `catalog_category_entity_varchar`(`value_id`,`entity_type_id`,`attribute_id`,`store_id`,`entity_id`,`value`) 
+values
+(1, 3, 41, 0, 1, 'Root Catalog'),
+(2, 3, 41, 1, 1, 'Root Catalog'),
+(3, 3, 43, 1, 1, 'root-catalog'),
+(4, 3, 41, 0, 2, 'Default Category'),
+(5, 3, 41, 1, 2, 'Default Category'),
+(6, 3, 49, 1, 2, 'PRODUCTS'),
+(7, 3, 43, 1, 2, 'default-category');
