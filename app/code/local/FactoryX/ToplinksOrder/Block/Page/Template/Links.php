82cec2c7b3f8bf205b83b62f2a7ad1cbeb09386e
Raphael Petrini <raphael@factoryx.com.au>
2016-11-25 15:03:52 +0100

    add toplinksorder module

---
+++ app/code/local/FactoryX/ToplinksOrder/Block/Page/Template/Links.php
@@ -0,0 +1,117 @@
+<?php
+
+/**
+ * Class FactoryX_ToplinksOrder_Block_Page_Template_Links
+ */
+class FactoryX_ToplinksOrder_Block_Page_Template_Links extends Mage_Page_Block_Template_Links
+{
+
+    /**
+     * Change the position of an existing to make the layout customization easier
+     * @param string $url
+     * @param string $position
+     * @return $this
+     */
+    public function setPosition($url, $position)
+    {
+        $this->_validateSetPosition($url, $position);
+
+        // Get the link and delete it from the current position
+        foreach ($this->_links as $k => $v) {
+            if ($v->getUrl() == $url) {
+                $link = $this->_links[$k];
+                unset($this->_links[$k]);
+            }
+        }
+
+        if (isset($link)) {
+            // Add link to new position
+            $this->_addIntoPosition($link, $position);
+        }
+
+        return $this;
+    }
+
+    /**
+     * Rename the title and label of a top link
+     * @param string $url
+     * @param string $name
+     * @return $this
+     */
+    public function rename($url, $name)
+    {
+        $this->_validateRename($url, $name);
+
+        foreach ($this->_links as $k => $v) {
+            if ($v->getUrl() == $url) {
+                // Get the link
+                $link = $this->_links[$k];
+                // Change the label and title
+                $link->setLabel($name);
+                $link->setTitle($name);
+                // Reassign the renamed link
+                $this->_links[$k] = $link;
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * Validate the parameters
+     * @param $url
+     * @param $name
+     * @throws Exception
+     */
+    protected function _validateRename($url, $name)
+    {
+        if (is_null($url)) {
+            throw new Exception(Mage::helper('toplinksorder')->__('Unable to rename top link because the url cannot be null'));
+        } elseif (is_array($url)) {
+            throw new Exception(Mage::helper('toplinksorder')->__('Unable to rename top link because the url cannot be an array'));
+        } elseif ($url === false) {
+            throw new Exception(Mage::helper('toplinksorder')->__('Unable to rename top link because the url cannot be false'));
+        } elseif (is_numeric($url)) {
+            throw new Exception(Mage::helper('toplinksorder')->__('Unable to rename top link because the url cannot be numeric'));
+        }
+
+        if (is_null($name)) {
+            throw new Exception(Mage::helper('toplinksorder')->__('Unable to rename top link of %s because the label cannot be null', $url));
+        } elseif (is_array($name)) {
+            throw new Exception(Mage::helper('toplinksorder')->__('Unable to rename top link of %s because the label cannot be an array', $url));
+        } elseif ($name === false) {
+            throw new Exception(Mage::helper('toplinksorder')->__('Unable to rename top link of %s because the label cannot be false', $url));
+        }
+    }
+
+    /**
+     * Validate the parameters
+     * @param $url
+     * @param $position
+     * @throws Exception
+     */
+    protected function _validateSetPosition($url, $position)
+    {
+        if (is_null($url)) {
+            throw new Exception(Mage::helper('toplinksorder')->__('Unable to change position because the url cannot be null'));
+        } elseif (is_array($url)) {
+            throw new Exception(Mage::helper('toplinksorder')->__('Unable to change position because the url cannot be an array'));
+        } elseif ($url === false) {
+            throw new Exception(Mage::helper('toplinksorder')->__('Unable to change position because the url cannot be false'));
+        } elseif (is_numeric($url)) {
+            throw new Exception(Mage::helper('toplinksorder')->__('Unable to change position because the url cannot be numeric'));
+        }
+
+        if (is_null($position)) {
+            throw new Exception(Mage::helper('toplinksorder')->__('Unable to change position of %s because the position cannot be null', $url));
+        } elseif (is_array($position)) {
+            throw new Exception(Mage::helper('toplinksorder')->__('Unable to change position of %s because the position cannot be an array', $url));
+        } elseif ($position === false) {
+            throw new Exception(Mage::helper('toplinksorder')->__('Unable to change position of %s because the position cannot be false', $url));
+        } elseif (is_string($position) && !is_numeric($position)) {
+            throw new Exception(Mage::helper('toplinksorder')->__('Unable to change position of %s because the position cannot be a string', $url));
+        } elseif ($position < 0) {
+            throw new Exception(Mage::helper('toplinksorder')->__('Unable to change position of %s because the position cannot be negative', $url));
+        }
+    }
+}
---
+++ app/code/local/FactoryX/ToplinksOrder/Test/Unit/Block/Page/Template/LinksTest.php
@@ -0,0 +1,131 @@
+<?php
+
+class FactoryX_ToplinksOrder_Test_Unit_Block_Page_Template_LinksTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @dataProvider validPositionsProvider
+     */
+    public function testSetValidPosition($url, $position)
+    {
+        $block = $this->_createTemplateLinksBlockWithLink();
+
+        $block->setPosition($url, $position);
+
+        $this->assertEquals($this->_getThePositionFromTheFirstLinkOfTheBlock($block), $position);
+    }
+
+    public function validPositionsProvider()
+    {
+        return array(
+            'normal behavior'       => array('/test-url', 20),
+            'string position'       => array('/test-url', '20'),
+        );
+    }
+
+    /**
+     * @dataProvider invalidPositionsProvider
+     * @expectedException Exception
+     * @expectedExceptionMessageRegExp /Unable to change position \w+/
+     */
+    public function testSetInvalidPosition($url, $position)
+    {
+        $block = $this->_createTemplateLinksBlockWithLink();
+
+        $block->setPosition($url, $position);
+    }
+
+    public function invalidPositionsProvider()
+    {
+        return array(
+            'null position'         => array('/test-url', null),
+            'bool position'         => array('/test-url', false),
+            'non numeric position'  => array('/test-url', 'foo'),
+            'negative position'     => array('/test-url', -1),
+            'array position'        => array('/test-url', array()),
+            'null url'              => array(null, 20),
+            'bool url'              => array(false, 20),
+            'numeric url'           => array(20, 20),
+            'array url'             => array(array(), 20),
+        );
+    }
+
+    /**
+     * @dataProvider validLabelProvider
+     */
+    public function testValidRename($url, $label)
+    {
+        $block = $this->_createTemplateLinksBlockWithLink();
+
+        $block->rename($url, $label);
+
+        $link = $this->_getTheFirstLinkFromTheBlock($block);
+
+        $this->assertEquals($link->getLabel(), $label);
+        $this->assertEquals($link->getTitle(), $label);
+
+    }
+
+    public function validLabelProvider()
+    {
+        return array(
+            'normal behavior'       => array('/test-url', 'New Label'),
+            'numeric label'         => array('/test-url', 10),
+            'negative label'        => array('/test-url', -1),
+        );
+    }
+
+    /**
+     * @dataProvider invalidLabelProvider
+     * @expectedException Exception
+     * @expectedExceptionMessageRegExp /Unable to rename top link \w+/
+     */
+    public function testInvalidRename($url, $label)
+    {
+        $block = $this->_createTemplateLinksBlockWithLink();
+
+        $block->rename($url, $label);
+    }
+
+    public function invalidLabelProvider()
+    {
+        return array(
+            'null label'            => array('/test-url', null),
+            'boolean label'         => array('/test-url', false),
+            'array label'           => array('/test-url', array()),
+            'null url'              => array(null, 'New Label'),
+            'bool url'              => array(false, 'New Label'),
+            'numeric url'           => array(20, 'New Label'),
+            'array url'             => array(array(), 'New Label'),
+        );
+    }
+
+    private function _createTemplateLinksBlockWithLink()
+    {
+        $block = new FactoryX_ToplinksOrder_Block_Page_Template_Links();
+
+        $this->_addLink($block);
+
+        return $block;
+    }
+
+    private function _addLink(FactoryX_ToplinksOrder_Block_Page_Template_Links $block)
+    {
+        $block->addLink(
+            'Test Label',
+            '/test-url',
+            'Test Title'
+        );
+    }
+
+    private function _getTheFirstLinkFromTheBlock(FactoryX_ToplinksOrder_Block_Page_Template_Links $block)
+    {
+        $links = $block->getLinks();
+        return current($links);
+    }
+
+    private function _getThePositionFromTheFirstLinkOfTheBlock(FactoryX_ToplinksOrder_Block_Page_Template_Links $block)
+    {
+        $links = $block->getLinks();
+        return key($links);
+    }
+}
