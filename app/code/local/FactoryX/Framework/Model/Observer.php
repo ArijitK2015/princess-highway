dca0ee0fe28954e0d8749e758281fc3bf90f85a7
Raphael Petrini <raphael@factoryx.com.au>
2016-12-08 16:36:33 +0100

    fix framework

--- app/code/local/FactoryX/Framework/Model/Observer.php
+++ app/code/local/FactoryX/Framework/Model/Observer.php
@@ -9,54 +9,12 @@ class FactoryX_Framework_Model_Observer
      * Add bootstrap classes to HTML on the fly
      * @param Varien_Event_Observer $observer
      */
-    public function addBootstrapClasses(Varien_Event_Observer $observer)
+    public function modifyOutput(Varien_Event_Observer $observer)
     {
         // Get the rendered block
         $block = $observer->getBlock();
 
-        if ($block instanceof Fontis_Australia_Block_Autocomplete) {
-            $newHtml = $this->_addUnstyledLink($observer->getTransport()->getHtml());
-        } else if ($block instanceof Mage_Wishlist_Block_Customer_Sidebar) {
-            $newHtml = $this->_addBadge(array('<small>(',')</small>'),array('<span class="badge">','</span>'),$observer->getTransport()->getHtml());
-        } else if ($block instanceof Mage_Wishlist_Block_Links) {
-            $newHtml = $observer->getTransport()->getHtml();
-            if (Mage::helper('framework')->isWishlistBadge()) {
-                $newHtml = $this->_addBadge(array('>My Wishlist (',' item)<'),array(">My Wishlist <span class='badge'>",'</span><'),$newHtml);
-                $newHtml = $this->_addBadge(array('>My Wishlist (',' items)<'),array(">My Wishlist <span class='badge'>",'</span><'),$newHtml);
-            } else {
-                $newHtml = str_replace(' item)',')',$newHtml);
-                $newHtml = str_replace(' items)',')',$newHtml);
-            }
-            if ($afterText = Mage::helper('framework')->getWishlistAfter()) {
-                $newHtml = str_replace('>My Wishlist','>My Wishlist ' . $afterText,$newHtml);
-            }
-            if ($text = Mage::helper('framework')->getWishlistText()) {
-                $newHtml = str_replace('My Wishlist',$text,$newHtml);
-            }
-        } else if ($block instanceof MageWorx_XSitemap_Block_Page_Template_Links
-            || $block instanceof Mage_Page_Block_Template_Links
-            || $block instanceof FactoryX_ToplinksOrder_Block_Page_Template_Links) {
-            $newHtml = $observer->getTransport()->getHtml();
-            if (Mage::helper('framework')->isCartBadge()) {
-                $newHtml = $this->_addBadge(array('>My Cart (',' item)<'),array(">My Cart <span class='badge'>",'</span><'),$newHtml);
-                $newHtml = $this->_addBadge(array('>My Cart (',' items)<'),array(">My Cart <span class='badge'>",'</span><'),$newHtml);
-            } else {
-                $newHtml = str_replace(' item)',')',$newHtml);
-                $newHtml = str_replace(' items)',')',$newHtml);
-            }
-            if ($afterText = Mage::helper('framework')->getCartAfter()) {
-                $newHtml = str_replace('>My Cart','>My Cart ' . $afterText,$newHtml);
-            }
-            if ($text = Mage::helper('framework')->getCartText()) {
-                $newHtml = str_replace('My Cart',$text,$newHtml);
-            }
-        } else if ($block instanceof Mage_CatalogSearch_Block_Autocomplete) {
-            $newHtml = $this->_addUnstyledLink($observer->getTransport()->getHtml());
-            $newHtml = str_replace('"amount"','"badge"',$newHtml);
-            $newHtml = str_replace('</span>','</span> ',$newHtml);
-        } else {
-            $newHtml = false;
-        }
+        $newHtml = $this->addBootstrapClasses($observer, $block);
 
         if ($newHtml) {
             // Change output HTML
@@ -82,5 +40,79 @@ class FactoryX_Framework_Model_Observer
     protected function _addBadge($toReplace,$badges,$html)
     {
         return str_replace($toReplace,$badges,$html);
+    }
+
+    /**
+     * @param Varien_Event_Observer $observer
+     * @param $block
+     * @return mixed
+     */
+    protected function addBootstrapClasses(Varien_Event_Observer $observer, $block)
+    {
+        if ($block instanceof Fontis_Australia_Block_Autocomplete) {
+            $newHtml = $this->_addUnstyledLink($observer->getTransport()->getHtml());
+            return $newHtml;
+        } else {
+            if ($block instanceof Mage_Wishlist_Block_Customer_Sidebar) {
+                $newHtml = $this->_addBadge(array('<small>(', ')</small>'), array('<span class="badge">', '</span>'),
+                    $observer->getTransport()->getHtml());
+                return $newHtml;
+            } else {
+                if ($block instanceof Mage_Wishlist_Block_Links) {
+                    $newHtml = $observer->getTransport()->getHtml();
+                    if (Mage::helper('framework')->isWishlistBadge()) {
+                        $newHtml = $this->_addBadge(array('>My Wishlist (', ' item)<'),
+                            array(">My Wishlist <span class='badge'>", '</span><'), $newHtml);
+                        $newHtml = $this->_addBadge(array('>My Wishlist (', ' items)<'),
+                            array(">My Wishlist <span class='badge'>", '</span><'), $newHtml);
+                    } else {
+                        $newHtml = str_replace(' item)', ')', $newHtml);
+                        $newHtml = str_replace(' items)', ')', $newHtml);
+                    }
+                    if ($afterText = Mage::helper('framework')->getWishlistAfter()) {
+                        $newHtml = str_replace('>My Wishlist', '>My Wishlist ' . $afterText, $newHtml);
+                    }
+                    if ($text = Mage::helper('framework')->getWishlistText()) {
+                        $newHtml = str_replace('My Wishlist', $text, $newHtml);
+                        return $newHtml;
+                    }
+                    return $newHtml;
+                } else {
+                    if ($block instanceof MageWorx_XSitemap_Block_Page_Template_Links
+                        || $block instanceof Mage_Page_Block_Template_Links
+                        || $block instanceof FactoryX_ToplinksOrder_Block_Page_Template_Links
+                    ) {
+                        $newHtml = $observer->getTransport()->getHtml();
+                        if (Mage::helper('framework')->isCartBadge()) {
+                            $newHtml = $this->_addBadge(array('>My Cart (', ' item)<'),
+                                array(">My Cart <span class='badge'>", '</span><'), $newHtml);
+                            $newHtml = $this->_addBadge(array('>My Cart (', ' items)<'),
+                                array(">My Cart <span class='badge'>", '</span><'), $newHtml);
+                        } else {
+                            $newHtml = str_replace(' item)', ')', $newHtml);
+                            $newHtml = str_replace(' items)', ')', $newHtml);
+                        }
+                        if ($afterText = Mage::helper('framework')->getCartAfter()) {
+                            $newHtml = str_replace('>My Cart', '>My Cart ' . $afterText, $newHtml);
+                        }
+                        if ($text = Mage::helper('framework')->getCartText()) {
+                            $newHtml = str_replace('My Cart', $text, $newHtml);
+                            return $newHtml;
+                        }
+                        return $newHtml;
+                    } else {
+                        if ($block instanceof Mage_CatalogSearch_Block_Autocomplete) {
+                            $newHtml = $this->_addUnstyledLink($observer->getTransport()->getHtml());
+                            $newHtml = str_replace('"amount"', '"badge"', $newHtml);
+                            $newHtml = str_replace('</span>', '</span> ', $newHtml);
+                            return $newHtml;
+                        } else {
+                            $newHtml = false;
+                            return $newHtml;
+                        }
+                    }
+                }
+            }
+        }
     }
 }
